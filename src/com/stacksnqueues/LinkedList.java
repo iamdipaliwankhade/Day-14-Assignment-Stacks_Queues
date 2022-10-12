c72f8a903dac7c98b491e45b0090d4501fb8bf4b
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-10-12 11:55:06 +0530

    [Dipali] Added : create stack

---
+++ src/com/stacksnqueues/LinkedList.java
@@ -0,0 +1,49 @@
+package com.stacksnqueues;
+/**
+ * 
+ * @author Dipali
+ *
+ */
+public class LinkedList {
+	Node head;
+
+	class Node {
+		Object data;
+		Node ref;
+
+		Node(Object data) {
+			this.data = data;
+		}
+	}
+
+	public void addLast(Object data) {
+		Node newNode = new Node(data);
+		Node temp = head;
+		if (head == null)
+			head = newNode;
+		else if (head.ref == null)
+			head.ref = newNode;
+		else {
+			while (temp.ref != null) {
+				temp = temp.ref;
+			}
+			temp.ref = newNode;
+		}
+	}
+
+	public void display() {
+		Node temp = head;
+		if (head == null)
+			System.out.println("No elements to show.");
+		else {
+			while (temp != null) {
+				if (temp.ref != null)
+					System.out.print(temp.data + " > ");
+				else
+					System.out.println(temp.data);
+
+				temp = temp.ref;
+			}
+		}
+	}
+}
